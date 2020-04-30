ab5b24ea0d6af8359d8e21fab7d00f46144ac424
Layla <layla.rowen@gmail.com>
2020-04-30 10:15:59 -0700

    Presentational and Container Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,44 +1,44 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
-
 class CampsiteInfo extends Component {
-    renderComments(comments){
-        if(comments){
-            return(
-            <div key={comments.id} className="col-md-5 m-1">
-                <h4>Comments</h4>
-                {comments.map(comment=><div>
-                                            <div>{comment.text}</div>
-                                            <p>--{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p></div>)}
-            </div>
+    renderComments(comments) {
+        if (comments) {
+            return (
+                <div key={comments.id} className="col-md-5 m-1">
+                    <h4>Comments</h4>
+                    {comments.map(comment => <div>
+                        <div>{comment.text}</div>
+                        <p>--{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p></div>)}
+                </div>
             );
         }
         return <div />;
     }
     renderCampsite(campsite) {
-            return(
-                <div className="col-md-5 m-1">
-                    <Card>
-                    <CardImg top width="100%" src={campsite.image} alt={campsite.name}/>
+        return (
+            <div className="col-md-5 m-1">
+                <Card>
+                    <CardImg top width="100%" src={campsite.image} alt={campsite.name} />
                     <CardBody>
                         <CardTitle>{campsite.name}</CardTitle>
                         <CardText>{campsite.description}</CardText>
                     </CardBody>
-                    </Card>
-                </div>
-                
-            );
-        }
+                </Card>
+            </div>
 
-    render(){
-        if(this.props.campsite){
+        );
+    }
+
+    render() {
+        if (this.props.campsite) {
             return (
-                <div className="row">
-                    {this.renderCampsite(this.props.campsite)}
-                    {this.renderComments(this.props.campsite.comments)}
+                <div className="container">
+                    <div className="row">
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
                 </div>
-                
             )
         }
         return <div />;
